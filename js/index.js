88116fc358011350a4df088f151a4aec10540ba1
PIASH <pjr@pjrs-MacBook-Air.local>
2023-03-26 23:29:57 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-const tabLinks = document.getElementsByClassName('tab-links');
-const tabContents = document.getElementsByClassName('tab-contents');
+var tabLinks = document.getElementsByClassName('tab-links');
+var tabContents = document.getElementsByClassName('tab-contents');
 
 function openTab(tabName){
-    for(tabLink of tabLinks){
+    for(const tabLink of tabLinks){
         tabLink.classList.remove("active-link");
     }
-    for(tabContent of tabContents){
+    for(const tabContent of tabContents){
         tabContent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 }
+
